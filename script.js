4bf2aa8990f9169a0e2d732ddb4111f851d83bd3
CharlotteConstant <constantcharlotte8@orange.fr>
2021-04-22 16:19:54 +0200

    animations bloc

--- script.js
+++ script.js
@@ -1,4 +1,72 @@
+"use strict";
+
 //header animation
 $(".hover").mouseleave(function () {
   $(this).removeClass("hover");
+});
+
+// animation au scroll
+const sr = ScrollReveal({
+  reset: false,
+  mobile: true,
+});
+
+sr.reveal(".logocharlotte", {
+  rotate: {
+    x: 0,
+    y: 90,
+    z: 0,
+  },
+  duration: 1500,
+});
+
+sr.reveal(".photoInfo", {
+  origin: "left",
+  distance: "300px",
+  duration: 2000,
+  //delay: 500,
+});
+
+sr.reveal(".btn", {
+  rotate: {
+    x: 90,
+    y: 0,
+    z: 0,
+  },
+  duration: 1500,
+  interval: 500,
+});
+
+sr.reveal(".emploi1", {
+  origin: "left",
+  distance: "300px",
+  duration: 2000,
+  delay: 500,
+});
+
+sr.reveal(
+  ".emploi2",
+  {
+    origin: "right",
+    distance: "300px",
+    duration: 2000,
+    delay: 500,
+  },
+  200
+);
+
+sr.reveal(
+  ".emploi3",
+  {
+    origin: "left",
+    distance: "300px",
+    duration: 2000,
+    delay: 500,
+  },
+  200
+);
+
+sr.reveal(".card", {
+  delay: 500,
+  interval: 400,
 });
